6e97055361fe8b0f3e31c0fcfc309ef1348e77be
sahirbhatnagar <sahir.bhatnagar@gmail.com>
2015-03-28 00:10:15 -0400

    added discussion of Ben Sherwood

--- discussion_sy.R
+++ discussion_sy.R
@@ -19,17 +19,17 @@ dat2 <- rbind(m1,m3,m5)
 #histograms of three most chosen models
 ggplot(dat2, aes(muhat, fill=model)) + geom_histogram(alpha = .8, position = 'identity')+
     geom_vline(xintercept = muhat.obs, colour="red", linetype = "longdash")+
-    geom_vline(xintercept = muhat.obs, colour="black", size=1.5)+
+    geom_vline(xintercept = muhat.obs, colour="black", size=1)+
     labs(x=expression(paste("fitted value   ",hat(mu)[1])), y="frequency")+
     annotate("text", label = "original estimate = 2.71\n based on Cp chosen model", x = -13, y = 210, 
-             size = 5, colour = "red")+
+             size = 4, colour = "red")+
     geom_segment(aes(x = -2.51, y = 210, xend = 2.71, yend = 210),
                  color="red", arrow = arrow(angle=20,length = unit(0.5, "cm")))+
     annotate("text", label = paste0(as.numeric(round(perc.chosen["m1"]*100,0)),"%"), 
-             x = -15, y = 75, size = 6, colour = "red")+
+             x = -15, y = 75, size = 4, colour = "red")+
     annotate("text", label = paste0(as.numeric(round(perc.chosen["m3"]*100,0)),"%"), 
-             x = -5, y = 75, size = 6, colour = "red")+
+             x = -5, y = 75, size = 4, colour = "red")+
     annotate("text", label = paste0(as.numeric(round(perc.chosen["m5"]*100,0)),"%"), 
-             x = 8, y = 75, size = 6, colour = "red") + xlim(c(-30,30))
+             x = 8, y = 75, size = 4, colour = "red") + xlim(c(-30,30))
 
 
